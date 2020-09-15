c8c54f43bcaa039ec43e10ad076ff1ef5c65c0c5
Shcherbak Artem <shherbak8909001@gmail.com>
2020-09-16 02:09:09 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const progress1 = document.querySelector('.progress1-done');
+
+setTimeout(() => {
+    progress1.style.opacity = 1;
+    progress1.style.width = progress1.getAttribute('data-done') + '%';
+}, 500)
+
+const progress2 = document.querySelector('.progress2-done');
+
+setTimeout(() => {
+    progress2.style.opacity = 1;
+    progress2.style.width = progress2.getAttribute('data-done') + '%';
+}, 500)
+
+const progress3 = document.querySelector('.progress3-done');
+
+setTimeout(() => {
+    progress3.style.opacity = 1;
+    progress3.style.width = progress3.getAttribute('data-done') + '%';
+}, 500)
+
+
+const progress4 = document.querySelector('.progress4-done');
+
+setTimeout(() => {
+    progress4.style.opacity = 1;
+    progress4.style.width = progress4.getAttribute('data-done') + '%';
+}, 500)
+
+
+const progress5 = document.querySelector('.progress5-done');
+
+setTimeout(() => {
+    progress5.style.opacity = 1;
+    progress5.style.width = progress5.getAttribute('data-done') + '%';
+}, 500)
+
+
+const progress6 = document.querySelector('.progress6-done');
+
+setTimeout(() => {
+    progress6.style.opacity = 1;
+    progress6.style.width = progress6.getAttribute('data-done') + '%';
+}, 500)
+
+const progress7 = document.querySelector('.progress7-done');
+
+setTimeout(() => {
+    progress7.style.opacity = 1;
+    progress7.style.width = progress7.getAttribute('data-done') + '%';
+}, 500)
+
+const progress8 = document.querySelector('.progress8-done');
+
+setTimeout(() => {
+    progress8.style.opacity = 1;
+    progress8.style.width = progress8.getAttribute('data-done') + '%';
+}, 500)
+const progress9 = document.querySelector('.progress9-done');
+
+setTimeout(() => {
+    progress9.style.opacity = 1;
+    progress9.style.width = progress9.getAttribute('data-done') + '%';
+}, 500)
+
